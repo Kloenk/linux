3b8c115c18f7a9326b35cb4dece6a84dde4c9d22
Finn Behrens <me@kloenk.de>
2021-04-30 14:34:42 +0200

    drivers/net: add wireguard_rs implementation
    Signed-off-by: Finn Behrens <me@kloenk.de>

---
+++ drivers/net/wireguard-rs/wireguard_rs.rs
@@ -0,0 +1,30 @@
+
+// SPDX-License-Identifier: GPL-2.0
+
+// TODO: crate doc
+
+#![no_std]
+#![feature(allocator_api, global_asm)]
+
+use core::ops::Deref;
+
+use kernel::prelude::*;
+use kernel::net::prelude::*;
+
+module! {
+    type: WireguardRs,
+    name: b"wireguard_rs",
+    author: b"Rust for Linux Contributors",
+    description: b"Rust Wireguard network driver",
+    license: b"GPL v2",
+    alias_rtnl_lock: b"wireguard_rs",
+}
+
+struct WireguardRs;
+
+impl KernelModule for WireguardRs {
+    fn init() -> KernelResult<Self> {
+
+        Ok(WireguardRs)
+    }
+}
